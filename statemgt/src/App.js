ae0877bd6571e5d917231b1b4ffd93b0a8f8d2b6
dheerajmehta <dheeraj_fw11_310@masai.school>
2022-01-12 21:07:01 +0530

    1

--- statemgt/src/App.js
+++ statemgt/src/App.js
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const [counter ,setCounter]= React.useState(5)
+  const [counter ,setCounter]= React.useState(1)
 
   const handleIncre = (value) => {
     setCounter(value+counter)
